db40b741071509bf89a43629535199b2f60ff30a
JoseAugusto83 <ja834136@gmail.com>
2024-08-19 17:44:40 +0000

    delete & put

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -1,8 +1,13 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +30,22 @@ public class AlunoController {
     @PostMapping
     public Aluno postAluno(@RequestBody Aluno aluno){
         return alunoRepo.save(aluno);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Aluno> details(@PathVariable long id){
+        return alunoRepo.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Aluno put(@PathVariable long id, @RequestBody Aluno body){
+        Optional<Aluno> resultado = alunoRepo.findById(id);
+        resultado.get().setNome(body.getNome());
+        return alunoRepo.save(resultado.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        alunoRepo.deleteById(id);
     }
 }
